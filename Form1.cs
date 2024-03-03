d1f8287f4a00c39c9ce06d0adb4f605e4bcd24be
daws11 <dawskutel@gmail.com>
2024-03-04 04:09:13 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text; // Fetch text from textBox1
+            if (inputText.ToUpper() == "NAMA_PRAKTIKAN")
+            {
+                label1.Text = "Halo NAMA_PRAKTIKAN"; // Assign text to label1's Text property
+            }
+            else
+            {
+                label1.Text = "Input yang diterima: " + inputText; // Assign text to label1's Text property
+            }
+        }
+    }
+}
